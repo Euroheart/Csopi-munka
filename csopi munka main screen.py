f99aecb685b5aabe395bbec5a47afe3de3e236e2
Euro <72340733+Euroheart@users.noreply.github.com>
2021-11-17 15:16:42 +0100

    Add files via upload
    amogus sussy balls

---
+++ csopi munka main screen.py
@@ -0,0 +1,6 @@
+print("Én egy egyenlet kiszámoló masina vagyok")
+print("Nyomd le az "1"-est az Elsőfokú egyenlet mególdásához, Nyomd le a "2"-est a Másodfokú gyenlet megoldásához")
+
+
+1=Elsőfokú
+2=Másodikfokú
